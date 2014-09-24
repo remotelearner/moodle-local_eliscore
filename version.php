460d230fae37627baadd02a7121d632aff11089c
Brent Boghosian <brent.boghosian@remote-learner.net>
2014-09-24 15:56:52 -0400

    ELIS-8764: Moodle HQ requested changes for acceptance into Moodle Plugin DB

--- version.php
+++ version.php
@@ -28,4 +28,5 @@ defined('MOODLE_INTERNAL') || die();
 $plugin->version = 2014030701;
 $plugin->requires = 2013111801.11;  // Requires this Moodle version
 $plugin->release = '2.6.4.1 (Build: 20140826)';
+$plugin->maturity = MATURITY_STABLE;
 
