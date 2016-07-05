b6b7d3445ace8caa8252801159af75d2dfc46d9e
Brent Boghosian <brent.boghosian@remote-learner.net>
2016-07-05 16:50:12 -0400

    ELIS-9209: Fix ETL not recording course time.

--- plugins/etl/version.php
+++ plugins/etl/version.php
@@ -2,6 +2,6 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2015102200;
+$plugin->version = 2015102201;
 $plugin->release = '3.0.0.0 (Build: 20151022)';
 $plugin->component = 'eliscore_etl';
