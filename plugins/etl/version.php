9fd94e870760ba200226c2de04254736a515b363
Brent Boghosian <brent.boghosian@remote-learner.net>
2017-11-10 11:26:29 -0500

    ELIS-9478: Reduce eliscore_etl chunk size
    Also, prevent ETL table duration overflow by increasing size.

--- plugins/etl/version.php
+++ plugins/etl/version.php
@@ -2,6 +2,6 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2015102201;
+$plugin->version = 2015102202;
 $plugin->release = '3.0.4.1 (Build: 20160817)';
 $plugin->component = 'eliscore_etl';
