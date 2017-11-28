35acaa0f3c7c48154a3a323fc11ba004a3922811
Brent Boghosian <brent.boghosian@remote-learner.net>
2017-11-27 23:19:46 -0500

    ELIS-9479: Version releases 2.9.9.5 (Build: 20171127)

--- plugins/etl/version.php
+++ plugins/etl/version.php
@@ -3,4 +3,4 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version = 2015051102;
-$plugin->release = '2.9.6.1 (Build: 20160718)';
+$plugin->release = '2.9.9.5 (Build: 20171127)';
--- version.php
+++ version.php
@@ -27,6 +27,6 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->version = 2015051102;
 $plugin->requires = 2015051100.00; // Requires this Moodle version
-$plugin->release = '2.9.9.3 (Build: 20170622)';
+$plugin->release = '2.9.9.5 (Build: 20171127)';
 $plugin->maturity = MATURITY_STABLE;
 
